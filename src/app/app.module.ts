c383b5076adbd1ffcd9aa8dd1d904f8d241fcf05
Eduardo Sanz García <esanzgar@gmail.com>
2019-03-01 10:12:51 +0000

    fix: module needed AuthModule and StoreService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
+import { AuthModule } from "../auth/auth.module";
+
+import { StoreService } from "../store/store.service";
 
 import { AppComponent } from "./containers/app.component";
 import { HeaderComponent } from "./components/header/header.component";
@@ -9,8 +12,8 @@ import { NavComponent } from "./components/nav/nav.component";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, NavComponent],
-  imports: [BrowserModule, AppRoutingModule],
-  providers: [],
+  imports: [BrowserModule, AppRoutingModule, AuthModule],
+  providers: [StoreService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
